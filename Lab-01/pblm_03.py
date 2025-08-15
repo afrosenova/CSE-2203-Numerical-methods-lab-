daa74de72b0f41fc1c432d8816b94f2a9f860624
afrosenova <nova.international2015@gmail.com>
2025-08-15 11:47:36 +0600

    Add files via upload

---
+++ Lab-01/pblm_03.py
@@ -0,0 +1,9 @@
+#Check Positive or Negative
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("This number is Positive")
+elif number < 0:
+    print("This number is Negative")
+else:
+    print("This number is Zero")
